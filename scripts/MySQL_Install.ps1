17cad5c73c46ba8b1b18f95894b9dc3d17ede394
AbuAbbaad <syed.shabir@gmail.com>
2016-10-31 00:51:58 +0530

    ...

--- scripts/MySQL_Install.ps1
+++ scripts/MySQL_Install.ps1
@@ -30,16 +30,16 @@ if ($process.ExitCode -eq 0){
 }
 else {
     Write-Host "Installer exit code  $($process.ExitCode) for file  $($msifile)"
-	Exit $($process.ExitCode)
 }
+Exit $($process.ExitCode)
 
-Write-Host "Confuring MySQL Instance....."
-$process_config = Start-Process -verb RunAs -FilePath "$env:ProgramFiles\MySQL\MySQL Server 5.5\bin\mysqlinstanceconfig.exe" -ArgumentList "-i -q ServiceName=MySQL RootPassword=mysql Port=3306 Charset=utf8 /l `"$logPath`"" -PassThru -Wait
+#Write-Host "Configuring MySQL Instance....."
+#$process_config = Start-Process -verb RunAs -FilePath "$env:ProgramFiles\MySQL\MySQL Server 5.5\bin\mysqlinstanceconfig.exe" -ArgumentList "-i -q ServiceName=MySQL RootPassword=mysql Port=3306 Charset=utf8 /l `"$logPath`"" -PassThru -Wait
 
-if ($process_config.ExitCode -eq 0){
-    Write-Host "Confuring MySQL Instance Successfull"
-}
-else {
-    Write-Host "Confuring MySQL Instance Failed:  $($process_config.ExitCode)"
-}
-Exit $($process_config.ExitCode)
+#if ($process_config.ExitCode -eq 0){
+#    Write-Host "Confuring MySQL Instance Successfull"
+#}
+#else {
+#    Write-Host "Confuring MySQL Instance Failed:  $($process_config.ExitCode)"
+#}
+#Exit $($process_config.ExitCode)
