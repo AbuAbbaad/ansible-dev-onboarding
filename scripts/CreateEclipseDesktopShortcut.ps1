93a39752d29bbf19f694e424cb9d8884e31ca235
AbuAbbaad <syed.shabir@gmail.com>
2016-10-31 16:15:15 +0530

    ..

--- scripts/CreateEclipseDesktopShortcut.ps1
+++ scripts/CreateEclipseDesktopShortcut.ps1
@@ -1,5 +1,10 @@
+Param (
+	[parameter(mandatory=$true)]
+	[ValidateNotNullorEmpty ()]
+	[string]$TargetFile
+	)
 # Create a Shortcut with Windows PowerShell
-$TargetFile = "$ENV:UserProfile\eclipse\eclipse.exe"
+#$TargetFile = "$ENV:UserProfile\eclipse\eclipse.exe"
 $ShortcutFile = "$env:Public\Desktop\eclipse.lnk"
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
